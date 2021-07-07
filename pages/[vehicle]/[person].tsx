81f674f9561ae8d7d7114c0e80a0021ab0e6ca5f
Labiba Ibnat <labiba.ssp@gmail.com>
2021-07-07 13:47:56 +0600

    feat: add vehicles list based on persons

---
+++ pages/[vehicle]/[person].tsx
@@ -0,0 +1,53 @@
+import { NextPageContext } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { VehiclePerson } from '../../type';
+import fetch from 'isomorphic-unfetch';
+
+export interface PersonProps {
+  ownersList?: VehiclePerson[];
+}
+
+export default function Person({ ownersList }: PersonProps) {
+  const router = useRouter();
+
+  const [owners, setOwners] = useState(ownersList);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch('http://localhost:4001/vehicles?ownerName=' + router.query.person + '&vehicle=' + router.query.vehicle);
+      const ownersList: VehiclePerson[] | undefined = await response.json();
+      setOwners(ownersList);
+    }
+
+    if (ownersList?.length == 0) {
+      loadData();
+    }
+  }, []);
+
+  if (!owners?.[0]) {
+    return <div>loading...</div>
+  }
+
+  return <pre>{owners[0]?.details}</pre>
+}
+
+interface MyNextPageContext extends NextPageContext {
+  query: {
+    person: string;
+    vehicle: string;
+  }
+}
+
+Person.getInitialProps = async (ctx: MyNextPageContext) => {
+  if (!ctx.req) {
+    return { ownersList: [] };
+  }
+
+  const { query } = ctx;
+
+  const response = await fetch('http://localhost:4001/vehicles?ownerName=' +
+    query.person + '&vehicle=' + query.vehicle);
+  const ownersList: VehiclePerson[] | undefined = await response.json();
+  return { ownersList: ownersList }
+}
